f5c950b8f08ebfe8784ae63639bf97fc37ddf7dd
Muhammad-Shafaf123 <shafafmuhammad6@gmail.com>
2023-08-12 23:24:11 +0530

    insert demmy data to db, no errors, page - 107

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql" // New import
+	// import models from internal/models
+	"snippetbox.shafaf.net/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 /*
@@ -18,6 +21,7 @@ we'll add more to it as the build progresses.
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -28,6 +32,7 @@ func main() {
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// To keep the main() function tidy I've put the code for creating a connection
 	// pool into the separate openDB() function below. We pass openDB() the DSN
 	// from the command-line flag.
@@ -35,18 +40,25 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+
 	// We also defer a call to db.Close(), so that the connection pool is closed
 	// before the main() function exits.
 	defer db.Close()
+
+	// Initialize a models.SnippetModel instance and add it to the application
+	// dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
+
 	infoLog.Printf("Starting server on %s", *addr)
 	// Because the err variable is now already declared in the code above, we need
 	// to use the assignment operator = here, instead of the := 'declare and assign'
